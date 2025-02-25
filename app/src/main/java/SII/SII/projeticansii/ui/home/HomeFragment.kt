a4bab03967fa4b18b82f56646c5c1969788370ca
Pouplard Adam <adam.pouplard@gmail.com>
2025-02-25 11:49:09 +0100

    Appeler André

--- app/src/main/java/SII/SII/projeticansii/ui/home/HomeFragment.kt
+++ app/src/main/java/SII/SII/projeticansii/ui/home/HomeFragment.kt
@@ -8,6 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import SII.SII.projeticansii.databinding.FragmentHomeBinding
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 
 class HomeFragment : Fragment() {
 
@@ -32,6 +38,12 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val button: Button = binding.button
+        button.setOnClickListener {
+            callOnClick()
+        }
+
         return root
     }
 
@@ -39,4 +51,18 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun callOnClick() {
+        val phoneNumber = "tel:0613776499" // Remplacez par le numéro de téléphone souhaité
+        val intent = Intent(Intent.ACTION_CALL).apply {
+            data = Uri.parse(phoneNumber)
+        }
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            startActivity(intent)
+        } else {
+            // Demandez la permission à l'utilisateur
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CALL_PHONE), 1)
+        }
+    }
+
 }
