19b24ef214649614fc2f3bbb500bd1a20a1ddf2a
Tomy216 <andremartinscapitao@gmail.com>
2025-02-26 10:58:28 +0100

    Ajout de la navigation pour les contacts et les paramètres, et configuration des paramètres d'App Insights

--- app/src/main/java/SII/SII/projeticansii/ui/contacts/ContactsFragment.kt
+++ app/src/main/java/SII/SII/projeticansii/ui/contacts/ContactsFragment.kt
@@ -1,5 +1,6 @@
-package SII.SII.projeticansii.ui.notifications
+package SII.SII.projeticansii.ui.contacts
 
+import SII.SII.projeticansii.databinding.FragmentContactsBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import SII.SII.projeticansii.databinding.FragmentNotificationsBinding
+import SII.SII.projeticansii.ui.dashboard.ContactsViewModel
 
-class NotificationsFragment : Fragment() {
+class ContactsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentContactsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val dashboardViewModel =
+            ViewModelProvider(this).get(ContactsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentContactsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textContacts
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/SII/SII/projeticansii/ui/contacts/ContactsViewModel.kt
+++ app/src/main/java/SII/SII/projeticansii/ui/contacts/ContactsViewModel.kt
@@ -1,13 +1,13 @@
-package SII.SII.projeticansii.ui.notifications
+package SII.SII.projeticansii.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ContactsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is contacts Fragment"
     }
     val text: LiveData<String> = _text
 }
